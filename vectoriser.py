48f88a63aaf6a6e2535492c35c0aed2dc7471f1b
James Collins <jamesneilcollins@gmail.com>
2021-08-05 17:21:24 +1000

    Add vectoriser

---
+++ vectoriser.py
@@ -0,0 +1,21 @@
+from cv2 import cv2
+import sys, os, requests 
+import pose_estimator
+
+if __name__ == "__main__":
+    FILE_NAME = sys.argv[1]
+
+    #Check that the specified image exists.
+    file_path = 'content/' + FILE_NAME
+    if os.path.isfile(file_path):
+
+        #get points
+        estimator = pose_estimator.Estimator()
+        skeleton = estimator.skeleton(file_path)
+
+        #convert points into vector 
+        vectoriser = pose_estimator.Vectoriser()
+        vectoriser.get_slants(skeleton)
+
+    else:
+        print("No file named", FILE_NAME, "exists. Make sure it's spelled correctly and in 'content' folder.")
